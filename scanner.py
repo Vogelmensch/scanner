c6b0791ecbaa3bbc3d6ccda99fe428b5e08ac7d3
David <davidknoepp@protonmail.com>
2025-04-25 17:02:02 +0200

    something is wrong with the fucking diagonals

--- scanner.py
+++ scanner.py
@@ -69,20 +69,34 @@ class Scanner:
         self.counter = 0
 
     # evaluate assignment by comparing it to the actual sensor_data
+    # return tuple (<bool> is_valid, <int> error_count)
     # lower is better; 0 is perfect.
     def eval_assignment(self, assignment):
+        # get the boolean values
+        assignment = np.vectorize(lambda scanner_variable: scanner_variable.value)(assignment)
+
         # count True elements in rows
         assignment_horizontal = np.count_nonzero(assignment, axis=1)
 
         # count True elements in left-right-diagonals
         assignment_flipped = np.fliplr(assignment)
-        assignment_diagonal_lr = [np.count_nonzero(np.diagonal(assignment_flipped, offset)) for offset in range(self.height-1, -self.width, -1)]
+        assignment_diagonal_lr = np.array([np.count_nonzero(np.diagonal(assignment_flipped, offset)) for offset in range(self.height-1, -self.width, -1)])
 
         # count True elements in cols
         assignment_vertical = np.count_nonzero(assignment, axis=0)
 
         # count True elements in right-left-diagonals
-        assignment_diagonal_rl = [np.count_nonzero(np.diagonal(assignment, offset)) for offset in range(-(self.width-1), self.height)]
+        assignment_diagonal_rl = np.array([np.count_nonzero(np.diagonal(assignment, offset)) for offset in range(-(self.width-1), self.height)])
+        
+        # Check whether the assignment is valid
+        is_valid = not any([np.any(self.sensor_data_horizontal < assignment_horizontal), 
+                                np.any(self.sensor_data_vertical < assignment_vertical),
+                                np.any(self.sensor_data_diagonal_lr < assignment_diagonal_lr),
+                                np.any(self.sensor_data_diagonal_rl < assignment_diagonal_rl)])
+        
+        print(self.sensor_data_diagonal_lr)
+        print(assignment_diagonal_lr)
+        print(self.sensor_data_diagonal_lr < assignment_diagonal_lr)
 
         # differences between sensor data and current assignment
         diffs_horizontal = np.sum(np.abs(assignment_horizontal - self.sensor_data_horizontal))
@@ -90,7 +104,59 @@ class Scanner:
         diffs_diagonal_lr = np.sum(np.abs(assignment_diagonal_lr - self.sensor_data_diagonal_lr))
         diffs_diagonal_rl = np.sum(np.abs(assignment_diagonal_rl - self.sensor_data_diagonal_rl))
 
-        return diffs_horizontal + diffs_vertical + diffs_diagonal_lr + diffs_diagonal_rl
+        error_count = diffs_horizontal + diffs_vertical + diffs_diagonal_lr + diffs_diagonal_rl
+
+        return is_valid, error_count
+        
+
+    # recursive function
+    # returns False if matrix is impossible and True if it is the correct assignment
+    # idx is an integer 
+    def backtracking_search_helper(self, matrix, idx):
+        # evaluate matrix
+        is_valid, error_count = self.eval_assignment(matrix)
+
+        # check termination conditions
+        if not is_valid or idx > self.height * self.width:
+            return False 
+        if error_count == 0:
+            # assign the correct solution to the global matrix
+            self.matrix = matrix
+            return True
+        
+        # switch value of current element
+        x = idx % self.width
+        y = idx / self.width
+        element = matrix[x, y]
+        matrix_left = matrix.copy()
+        matrix_right = matrix.copy()
+        if not element.is_fixed:
+            matrix_left[x,y].value = False
+            matrix_right[x,y].value = True
+       
+        return self.backtracking_search_helper(matrix_left, idx+1) or self.backtracking_search_helper(matrix_right, idx+1)
+
+    # wrapper
+    def backtracking_search(self):
+        return self.backtracking_search_helper(self.matrix, 0)
+
+
+    def __str__(self):
+        str = ""
+        for row in np.vectorize(lambda b: ' ' if b.value == None else '#' if b.value else '.')(self.matrix):
+            for char in row:
+                print(char, end="")
+            print()
+        return str
+
+
+
+
+
+
+    # --- LOCAL SEARCH STUFF BELOW ---
+
+
 
     # change every cell and evaluate the new state
     # return the evaluations in a matrix
@@ -170,13 +236,7 @@ class Scanner:
 
             self.annealingStep(probabilityConstant)
 
-    def __str__(self):
-        str = ""
-        for row in np.vectorize(lambda b: ' ' if b.value == None else '#' if b.value else '.')(self.matrix):
-            for char in row:
-                print(char, end="")
-            print()
-        return str
+
 
 
 
@@ -195,8 +255,8 @@ sensor_data = [[10,10,6,4,6,8,13,15,11,6],[0,1,2,2,2,2,4,5,5,6,7,6,5,6,6,5,5,6,6
 
 scanner = Scanner(sensor_data)
 print(scanner)
-
-
+print(scanner.backtracking_search())
+print(scanner)
 
 
 
