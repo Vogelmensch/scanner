80b2e6cc78c29c18886c273b27e8b00b9264bc69
David <davidknoepp@protonmail.com>
2025-04-27 17:45:30 +0200

    added pure_scanner. Both scanners work now. But backtracking search is way to bad

--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 class ScannerVariable:
     def __init__(self, value = None, is_fixed=False):
@@ -80,23 +81,19 @@ class Scanner:
 
         # count True elements in left-right-diagonals
         assignment_flipped = np.fliplr(assignment)
-        assignment_diagonal_lr = np.array([np.count_nonzero(np.diagonal(assignment_flipped, offset)) for offset in range(self.height-1, -self.width, -1)])
+        assignment_diagonal_lr = np.array([np.count_nonzero(np.diagonal(assignment_flipped, offset)) for offset in range(self.width-1, 1-self.height-1, -1)])
 
         # count True elements in cols
         assignment_vertical = np.count_nonzero(assignment, axis=0)
 
         # count True elements in right-left-diagonals
-        assignment_diagonal_rl = np.array([np.count_nonzero(np.diagonal(assignment, offset)) for offset in range(-(self.width-1), self.height)])
-        
+        assignment_diagonal_rl = np.array([np.count_nonzero(np.diagonal(assignment, offset)) for offset in range(1-self.height, self.width)])
+
         # Check whether the assignment is valid
         is_valid = not any([np.any(self.sensor_data_horizontal < assignment_horizontal), 
                                 np.any(self.sensor_data_vertical < assignment_vertical),
                                 np.any(self.sensor_data_diagonal_lr < assignment_diagonal_lr),
                                 np.any(self.sensor_data_diagonal_rl < assignment_diagonal_rl)])
-        
-        print(self.sensor_data_diagonal_lr)
-        print(assignment_diagonal_lr)
-        print(self.sensor_data_diagonal_lr < assignment_diagonal_lr)
 
         # differences between sensor data and current assignment
         diffs_horizontal = np.sum(np.abs(assignment_horizontal - self.sensor_data_horizontal))
@@ -116,38 +113,53 @@ class Scanner:
         # evaluate matrix
         is_valid, error_count = self.eval_assignment(matrix)
 
+        print(self._pretty_str(matrix))
+
         # check termination conditions
-        if not is_valid or idx > self.height * self.width:
+        if not is_valid or idx >= self.height * self.width:
             return False 
         if error_count == 0:
             # assign the correct solution to the global matrix
             self.matrix = matrix
             return True
         
-        # switch value of current element
-        x = idx % self.width
-        y = idx / self.width
-        element = matrix[x, y]
-        matrix_left = matrix.copy()
-        matrix_right = matrix.copy()
-        if not element.is_fixed:
-            matrix_left[x,y].value = False
-            matrix_right[x,y].value = True
+        # set value of current element
+        matrix_flattened = matrix.reshape(-1)
+        element = matrix_flattened[idx]
+
+        # skip assignment if element is fixed
+        if element.is_fixed:
+            return self.backtracking_search_helper(matrix, idx+1)
+
+        # else, assign all possible values
+        matrix_left = deepcopy(matrix_flattened)
+        matrix_right = deepcopy(matrix_flattened)
+        matrix_left[idx].value = False
+        matrix_right[idx].value = True
        
-        return self.backtracking_search_helper(matrix_left, idx+1) or self.backtracking_search_helper(matrix_right, idx+1)
+        # recursive call; reshape left and right matrices to be of the original dimension again
+        return self.backtracking_search_helper(matrix_left.reshape((self.height, self.width)), idx+1) or self.backtracking_search_helper(matrix_right.reshape((self.height, self.width)), idx+1)
 
     # wrapper
     def backtracking_search(self):
-        return self.backtracking_search_helper(self.matrix, 0)
+        element = self.matrix[0,0]
+        matrix_left = deepcopy(self.matrix)
+        matrix_right = deepcopy(self.matrix)
+        if not element.is_fixed:
+            matrix_left[0,0].value = False
+            matrix_right[0,0].value = True
+        return self.backtracking_search_helper(matrix_left, 1) or self.backtracking_search_helper(matrix_right, 1)
 
-
-    def __str__(self):
+    def _pretty_str(self, matrix):
         str = ""
-        for row in np.vectorize(lambda b: ' ' if b.value == None else '#' if b.value else '.')(self.matrix):
+        for row in np.vectorize(lambda b: ' ' if b.value == None else '#' if b.value else '.')(matrix):
             for char in row:
                 print(char, end="")
             print()
         return str
+
+    def __str__(self):
+        return self._pretty_str(self.matrix)
 
 
 
@@ -187,12 +199,6 @@ class Scanner:
             return True
         else:
             return False
-
-    # do hillclimb until a local minimum is reached
-    def hillClimb(self):
-        eval = self.assignAndEvaluate()
-        while(self.changeMatrixFromEvaluation(eval)):
-            eval = self.assignAndEvaluate()
 
     def annealingStep(self, probabilityConstant):
         # 1. choose random neighbor of matrix (change one random element)
@@ -238,19 +244,8 @@ class Scanner:
 
 
 
-
-
-def hillClimbRandomRestart(sensor_data):    
-    eval = 1
-    while eval > 0:
-        scanner = Scanner(sensor_data)
-        scanner.hillClimb()
-        eval = scanner.currentEval
-        print(eval)
-    print(scanner)
-
-
 sensor_data = [[10,10,6,4,6,8,13,15,11,6],[0,1,2,2,2,2,4,5,5,6,7,6,5,6,6,5,5,6,6,3,2,2,1,0],[2,4,5,5,7,6,7,10,10,10,7,3,3,5,5],[0,0,1,3,4,4,4,4,3,4,5,7,8,8,9,9,6,4,4,2,0,0,0,0]]
+# sensor_data = [[3,2,2,2,1], [0,0,2,2,3,1,2,0,0], [0,2,4,3,1], [0,0,1,2,3,0,2,1,1]]
 # sensor_data = [[2,1,0],[1,0,2,0,0],[1,1,1],[0,0,2,0,1]]
 
 scanner = Scanner(sensor_data)
