b6c8dd2b81f3650df0821a414886caff6267280c
David <davidknoepp@protonmail.com>
2025-05-24 23:50:52 +0200

    n² -> 2^n

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ y2 = 6 * xs - 2
 
 plt.figure()
 plt.plot(xs, y2, label="$6n-2$")
-plt.plot(xs, y1, label="$n^2$")
+plt.plot(xs, y1, label="$2^n$")
 plt.xlabel("dimension $n$")
 plt.ylabel("number of variables / equations")
 plt.grid()
